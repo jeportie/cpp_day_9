0c4ab98c7c0e37dcacd1eded443850dfd160dabb
Jerome Portier <jeportie@f2r2s3.clusters.42paris.fr>
2025-06-30 14:40:24 +0200

    push pause

--- ex02/src/sort/sort_deque.cpp
+++ ex02/src/sort/sort_deque.cpp
@@ -21,18 +21,22 @@
 typedef std::pair<int,int>        PairII;
 typedef std::deque<PairII>       PairList;
 typedef std::deque<int>          IntDeq;
-typedef std::deque<size_t>       SizeVec;
+typedef std::deque<size_t>       SizeDeq;
 
-// 1) Split into (small,large) pairs; if odd, large=INT_MAX
 static PairList sortInPairs(const IntDeq& data)
 {
-    PairList pairs;
-    for (size_t i = 0; i < data.size(); )
+    PairList	pairs;
+	size_t		i;
+	int			a;
+	int			b;
+
+	i = 0;
+    while (i < data.size())
     {
-        int a = data[i++];
+        a = data[i++];
         if (i < data.size())
         {
-            int b = data[i++];
+            b = data[i++];
             if (a > b) std::swap(a,b);
             pairs.push_back(PairII(a,b));
         }
@@ -41,109 +45,129 @@ static PairList sortInPairs(const IntDeq& data)
             pairs.push_back(PairII(a, INT_MAX));
         }
     }
-    return pairs;
+    return (pairs);
 }
 
-// Compare by the 'large' element
 static bool compareByLarge(const PairII& A, const PairII& B)
 {
-    return A.second < B.second;
+    return (A.second < B.second);
 }
 
-// 2) Generate Jacobsthal‐order insertion indices  for n smalls
-static SizeVec generateInsertOrder(size_t n)
+static SizeDeq generateInsertOrder(size_t n)
 {
-    SizeVec order;
-    if (n <= 1) return order;
+    SizeDeq order;
+    SizeDeq J;
+	size_t	next;
+    size_t	covered;
+	size_t	i;
+	size_t	k;
+	size_t	lim;
 
-    // Build Jacobsthal numbers J(2)=3, J(3)=5, J(k)=J(k-1)+2*J(k-2)
-    SizeVec J; J.push_back(3); J.push_back(5);
+    if (n <= 1) return order;
+	J.push_back(3);
+	J.push_back(5);
     while (true)
     {
-        size_t next = J[J.size()-1] + 2*J[J.size()-2];
-        if (next > n) break;
+        next = J[J.size()-1] + 2*J[J.size()-2];
+        if (next > n)
+			break;
         J.push_back(next);
     }
-
-    // For each J[k] ≤ n, insert indices J[k]-1 down to previous+1
-    size_t covered=0;
-    for (size_t k=0; k < J.size() && J[k] <= n; ++k)
+    covered = 0;
+	k = 0;
+    while (k < J.size() && J[k] <= n)
     {
-        size_t lim = J[k] - 1;
-        for (size_t i = lim; i > covered; --i)
+        lim = J[k] - 1;
+		i = lim;
+		while (i > covered)
+		{
             order.push_back(i);
+			--i;
+		}
         covered = lim;
+		++k;
     }
-    // Fill in any remaining
-    for (size_t i = 0; i < n; ++i)
+	i = 0;
+    while (i < n)
+	{
         if (std::find(order.begin(), order.end(), i) == order.end())
             order.push_back(i);
+		++i;
+	}
     return order;
 }
 
-// 3) Insert each pending small value into 'out' by Jacobsthal order
-static void fj_insert_smalls(IntDeq& out, const IntDeq& sorted_larges,
-    const IntDeq& smalls, bool hasOdd, int oddValue)
+static void insert_smalls(IntDeq& out, const IntDeq& sorted_larges, const IntDeq& smalls, bool hasOdd, int oddValue)
 {
-    out.clear();
-    // 3a) seed: smallest small + all larges
-    out.push_back(smalls[0]);
-    for (size_t i = 0; i < sorted_larges.size(); ++i)
-        out.push_back(sorted_larges[i]);
+	size_t				i;
+	SizeDeq				order;
+	SizeDeq::size_type	j;
+	size_t				idx;
+	int					v;
+	IntDeq::iterator	pos;
 
-    // 3b) insert the rest of smalls in Jacobsthal order
-    SizeVec order = generateInsertOrder(smalls.size()-1);
-    // pending = smalls[1..]
-    for (SizeVec::size_type i = 0; i < order.size(); ++i)
+    out.clear();
+    out.push_back(smalls[0]);
+	i = 0;
+    while (i < sorted_larges.size())
+	{
+        out.push_back(sorted_larges[i]);
+		++i;
+	}
+    order = generateInsertOrder(smalls.size()-1);
+	j = 0;
+    while (j < order.size())
     {
-        size_t idx = order[i];
-        int v = smalls[idx + 1];  // +1 because we seeded with smalls[0]
-        IntDeq::iterator pos = binarySearch(out.begin(), out.end(), v);
+        idx = order[j];
+        v = smalls[idx + 1];
+        pos = binarySearch(out.begin(), out.end(), v);
         out.insert(pos, v);
+		++j;
     }
-    // 3c) finally, if odd, insert that singleton
     if (hasOdd)
     {
-        IntDeq::iterator pos = binarySearch(out.begin(), out.end(), oddValue);
+        pos = binarySearch(out.begin(), out.end(), oddValue);
         out.insert(pos, oddValue);
     }
 }
 
-// The driver: fully recursive Ford–Johnson sort on deque<int>
 void sortDeque(IntDeq& container)
 {
+    PairList	pairs;
+    bool		hasOdd;
+    int			oddVal;
+    IntDeq		smalls;
+	IntDeq		larges;
+    IntDeq		sorted;
+	size_t		i;
+
+	// recursion end gate
     if (container.size() < 2)
         return;
-
     // 1) Pair up & sort by their large half
-    PairList pairs = sortInPairs(container);
+    pairs = sortInPairs(container);
     std::sort(pairs.begin(), pairs.end(), compareByLarge);
-
     // 2) Detect & peel off odd sentinel
-    bool hasOdd = false;
-    int  oddVal = 0;
+    hasOdd = false;
+    oddVal = 0;
     if (!pairs.empty() && pairs.back().second == INT_MAX)
     {
         hasOdd = true;
-        oddVal  = pairs.back().first;
+        oddVal = pairs.back().first;
         pairs.pop_back();
     }
-
     // 3) Extract smalls[] and larges[]
-    IntDeq smalls, larges;
-    for (size_t i = 0; i < pairs.size(); ++i)
+	i = 0;
+    while (i < pairs.size())
     {
         smalls.push_back(pairs[i].first);
         larges.push_back(pairs[i].second);
+		++i;
     }
-
     // 4) Recurse on the larges
     sortDeque(larges);
-
     // 5) Merge-insert the smalls back
-    IntDeq sorted;
-    fj_insert_smalls(sorted, larges, smalls, hasOdd, oddVal);
-
+    insert_smalls(sorted, larges, smalls, hasOdd, oddVal);
     // 6) Replace original
     container.swap(sorted);
 }
